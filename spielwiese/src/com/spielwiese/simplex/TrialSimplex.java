fcfb6efca93ca85bd415c00789b34bf4f896587d
ctr <christian.trummer@chello.at>
2014-04-24 16:36:20 +0200

    Unbound problem unsolved ...

--- spielwiese/src/com/spielwiese/simplex/TrialSimplex.java
+++ spielwiese/src/com/spielwiese/simplex/TrialSimplex.java
@@ -11,7 +11,6 @@ import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
 import org.apache.commons.math3.optim.linear.Relationship;
 import org.apache.commons.math3.optim.linear.SimplexSolver;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 
 public class TrialSimplex {
 
@@ -24,14 +23,15 @@ public class TrialSimplex {
   private int[] pickAmount;
 
   public static void main(String[] args) throws Exception {
-    TrialSimplex trial = new TrialSimplex(10, 5, 15);
-    trial.testMath272();
-    trial.prepare();
+    TrialSimplex trial = new TrialSimplex(15, 3, 5);
+    //    trial.testMath272();
+    trial.optimizePblStationSlotting();
   }
 
-  private void prepare() {
-    createVector(numberOfProducts * numberOfLocations);
-  }
+  //
+  //  private void prepare() {
+  //    createVector(numberOfProducts * numberOfLocations);
+  //  }
 
   public TrialSimplex(int numberOfProducts, int numberOfPblLines, int numberOfLocationsPerPblLine) {
     this.numberOfProducts = numberOfProducts;
@@ -41,46 +41,65 @@ public class TrialSimplex {
     this.pickAmount = createExpectedPicksPerSku();
   }
 
-  public void testMath272() {
-    LinearObjectiveFunction f = new LinearObjectiveFunction(new double[] {2, 2, 1}, 0);
-    Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
-    constraints.add(new LinearConstraint(new double[] {1, 1, 0}, Relationship.GEQ, 1));
-    constraints.add(new LinearConstraint(new double[] {1, 0, 1}, Relationship.GEQ, 1));
-    constraints.add(new LinearConstraint(new double[] {0, 1, 0}, Relationship.GEQ, 1));
+  //  public void testMath272() {
+  //    LinearObjectiveFunction f = new LinearObjectiveFunction(new double[] {2, 2, 1}, 0);
+  //    Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
+  //    constraints.add(new LinearConstraint(new double[] {1, 1, 0}, Relationship.GEQ, 1));
+  //    constraints.add(new LinearConstraint(new double[] {1, 0, 1}, Relationship.GEQ, 1));
+  //    constraints.add(new LinearConstraint(new double[] {0, 1, 0}, Relationship.GEQ, 1));
+  //
+  //    LinearConstraintSet set = new LinearConstraintSet(constraints);
+  //
+  //    org.apache.commons.math3.optim.linear.SimplexSolver solver = new SimplexSolver();
+  //    PointValuePair solution = solver.optimize(f, set);
+  //
+  //    Assert.assertEquals(0.0, solution.getPoint()[0], .0000001);
+  //    Assert.assertEquals(1.0, solution.getPoint()[1], .0000001);
+  //    Assert.assertEquals(1.0, solution.getPoint()[2], .0000001);
+  //    Assert.assertEquals(3.0, solution.getValue(), .0000001);
+  //  }
+
+  public void optimizePblStationSlotting() {
+    LinearObjectiveFunction f = createObjectiveFunction();
+    Collection<LinearConstraint> constraints = createRestrictionOneLocationPerProduct();
+    constraints.addAll(createRestrictionOneSKUperLocation());
 
     LinearConstraintSet set = new LinearConstraintSet(constraints);
-
-    org.apache.commons.math3.optim.linear.SimplexSolver solver = new SimplexSolver();
+    SimplexSolver solver = new SimplexSolver();
     PointValuePair solution = solver.optimize(f, set);
 
-    Assert.assertEquals(0.0, solution.getPoint()[0], .0000001);
-    Assert.assertEquals(1.0, solution.getPoint()[1], .0000001);
-    Assert.assertEquals(1.0, solution.getPoint()[2], .0000001);
-    Assert.assertEquals(3.0, solution.getValue(), .0000001);
   }
 
   private Collection<LinearConstraint> createRestrictionOneSKUperLocation() {
     Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
-    double[] vector;
+    double[] vectorUpperLimit;
+    double[] vectorLowerLimit;
     for (int locationIndex = 1; locationIndex <= numberOfLocations; locationIndex++) {
-      vector = createVector(numberOfProducts * numberOfLocations - 1);
+      vectorUpperLimit = createVector(numberOfProducts * numberOfLocations);
+      vectorLowerLimit = createVector(numberOfProducts * numberOfLocations);
       for (int skuIndex = 1; skuIndex <= numberOfProducts; skuIndex++) {
-        vector[calculateIndex(skuIndex, locationIndex)] = 1.0;
+        vectorUpperLimit[calculateIndex(skuIndex, locationIndex)] = 1.0;
+        vectorLowerLimit[calculateIndex(skuIndex, locationIndex)] = 1.0;
       }
-      constraints.add(new LinearConstraint(vector, Relationship.LEQ, 1));
+      constraints.add(new LinearConstraint(vectorUpperLimit, Relationship.EQ, 1));
+      //      constraints.add(new LinearConstraint(vectorLowerLimit, Relationship.GEQ, 0));
     }
     return constraints;
   }
 
   private Collection<LinearConstraint> createRestrictionOneLocationPerProduct() {
     Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
-    double[] vector;
+    double[] vectorUpperLimit;
+    double[] vectorLowerLimit;
     for (int skuIndex = 1; skuIndex <= numberOfProducts; skuIndex++) {
-      vector = createVector(numberOfProducts * numberOfLocations - 1);
+      vectorUpperLimit = createVector(numberOfProducts * numberOfLocations);
+      vectorLowerLimit = createVector(numberOfProducts * numberOfLocations);
       for (int locationIndex = 1; locationIndex <= numberOfLocations; locationIndex++) {
-        vector[calculateIndex(skuIndex, locationIndex)] = 1.0;
+        vectorUpperLimit[calculateIndex(skuIndex, locationIndex)] = 1.0;
+        vectorLowerLimit[calculateIndex(skuIndex, locationIndex)] = 1.0;
       }
-      constraints.add(new LinearConstraint(vector, Relationship.LEQ, 1));
+      constraints.add(new LinearConstraint(vectorUpperLimit, Relationship.EQ, 1));
+      //      constraints.add(new LinearConstraint(vectorLowerLimit, Relationship.GEQ, 1));
 
     }
     return constraints;
@@ -93,12 +112,15 @@ public class TrialSimplex {
 
     double[] vector = createVector(numberOfProducts * numberOfLocations);
     for (int skuIndex = 1; skuIndex <= numberOfProducts; skuIndex++) {
-
+      for (int locationIndex = 1; locationIndex <= numberOfLocations; locationIndex++) {
+        double value = pickAmount[skuIndex - 1] * calculatePickEffort(locationIndex);
+        vector[calculateIndex(skuIndex, locationIndex)] = value;
+      }
     }
 
-    LinearObjectiveFunction f = new LinearObjectiveFunction(new double[] {2, 2, 1}, 0);
+    LinearObjectiveFunction f = new LinearObjectiveFunction(vector, 0);
 
-    return null;
+    return f;
   }
 
   private int[] createExpectedPicksPerSku() {
@@ -109,20 +131,25 @@ public class TrialSimplex {
     return pickAmounts;
   }
 
-  private double calculatePickEffort(int PblLine, int PblLocationInLine) {
-    int relLine = Math.abs(PblLine - (numberOfPblLines / 2));
-    int relLoc = Math.abs(PblLocationInLine - (numberOfLocations / 2));
+  private double calculatePickEffort(int locationIndex) {
+    int pblLine = locationIndex / numberOfPblLines;
+    int pblLocationInLine = locationIndex % numberOfPblLines;
+
+    int relLine = Math.abs(pblLine - (numberOfPblLines / 2));
+    int relLoc = Math.abs(pblLocationInLine - (numberOfLocations / 2));
 
     return relLine + relLoc;
   }
 
   private int calculateIndex(int skuIndex, int locationIndex) {
-    return ((skuIndex - 1) * numberOfLocations + locationIndex) - 1;
+    int index = (skuIndex - 1) * numberOfLocations + locationIndex - 1;
+    logger.debug("calculateIndex == " + index);
+    return index;
   }
 
   private double[] createVector(int size) {
     double[] vector = new double[size];
-    logger.debug("vector initial is: " + vectorToString(vector));
+    logger.debug("size of created vector is == " + vector.length);
     return vector;
   }
 
