49860e4d9a1188134ef40d099c5aab902f10099a
ctrummer <c.trummer@ssi-schaefer-peem.com>
2014-10-01 13:42:19 +0200

    Mac version of classpath (ActiveMQ/Kafka Example Running)

--- ActiveMQTest/src/TestStarter.java
+++ ActiveMQTest/src/TestStarter.java
@@ -4,79 +4,79 @@ import java.io.InputStreamReader;
 
 public class TestStarter {
 
-	private final static String classpath = "./bin;" + //
-			"./lib/geronimo-jms_1.1_spec-1.1.jar;" + //
-			"./lib/qpid-amqp-1-0-client-0.26.jar;" + //
-			"./lib/qpid-amqp-1-0-client-jms-0.26.jar;" + //
-			"./lib/qpid-amqp-1-0-common-0.26.jar;";
+    private final static String baseDir = "/Users/ctrummer/git/projects/ActiveMQTest/";
 
-	public static void main(String[] args) throws IOException,
-			InterruptedException {
-		OutputWriter out = new OutputWriter("./log/TestStarter.txt");
-		out.writeln("TestStarter started.");
+    private final static String classpath = "./:" + "./bin/:" + // baseDir + ";" + baseDir + "bin/;"
+                                                                // +
+                                                                // //
+            "./lib/geronimo-jms_1.1_spec-1.1.jar:" + //
+            "./lib/qpid-amqp-1-0-client-0.26.jar:" + //
+            "./lib/qpid-amqp-1-0-client-jms-0.26.jar:" + //
+            "./lib/qpid-amqp-1-0-common-0.26.jar:";
 
-		for (int listenerCounter = 1; listenerCounter <= Configuration.numberOfSubsribers; listenerCounter++) {
-			if (listenerCounter != Configuration.numberOfSubsribers) {
-				createSubVm("java", "-cp", classpath, "Listener",
-						Configuration.destination_listener, "Listener"
-								+ listenerCounter, "all");
-			} else {
-				createSubVm("java", "-cp", classpath, "Listener",
-						Configuration.destination_listener, "Listener"
-								+ listenerCounter, "1");
-			}
+    public static void main(String[] args) throws IOException, InterruptedException {
+        OutputWriter out = new OutputWriter("./log/TestStarter.txt");
+        out.writeln("TestStarter started.");
 
-			out.writeln("Listener number " + listenerCounter + " started.");
-		}
+        System.out.println("cp == " + classpath);
 
-		// for (int publisherCounter = 1; publisherCounter <=
-		// Configuration.numberOfPublishers; publisherCounter++) {
-		// createSubVm("java", "-cp", classpath, "ListenerPublisher", ""
-		// + publisherCounter);
-		// System.out.println("Publisher number " + publisherCounter
-		// + " started.");
-		// }
-		//
-		// Thread.sleep(10000);
-		//
-		// createSubVm("java", "-cp", classpath, "PublishersStarter");
+        for (int listenerCounter = 1; listenerCounter <= Configuration.numberOfSubsribers; listenerCounter++) {
+            if (listenerCounter != Configuration.numberOfSubsribers) {
+                createSubVm("java", "-cp", classpath, "Listener",
+                        Configuration.destination_listener, "Listener" + listenerCounter, "all");
+            } else {
+                createSubVm("java", "-cp", classpath, "Listener",
+                        Configuration.destination_listener, "Listener" + listenerCounter, "1");
+            }
 
-		out.writeln("PublisherStarter started.");
-		out.writeln("TestStarter finished.");
-		out.close();
-	}
+            out.writeln("Listener number " + listenerCounter + " started.");
+        }
 
-	public static void createSubVm(String... args) throws IOException {
-		// ProcessBuilder builder = new ProcessBuilder("java", "-cp", classpath,
-		// args[0], args[1]);
-		ProcessBuilder builder = new ProcessBuilder(args);
-		builder.redirectErrorStream(true);
+        for (int publisherCounter = 1; publisherCounter <= Configuration.numberOfPublishers; publisherCounter++) {
+            createSubVm("java", "-cp", classpath, "ListenerPublisher", "" + publisherCounter);
+            System.out.println("Publisher number " + publisherCounter + " started.");
+        }
 
-		final Process p = builder.start();
+        Thread.sleep(10000);
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String line = null;
-				final BufferedReader reader = new BufferedReader(
-						new InputStreamReader(p.getInputStream()));
-				try {
-					while ((line = reader.readLine()) != null) {
-						System.out.println(line);
-					}
-					p.waitFor();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						reader.close();
-					} catch (IOException e) {
-						/** ignore on close */
-					}
-				}
-			}
-		}).start();
-	}
+        createSubVm("java", "-cp", classpath, "PublishersStarter");
+
+        out.writeln("PublisherStarter started.");
+        out.writeln("TestStarter finished.");
+        out.close();
+    }
+
+    public static void createSubVm(String... args) throws IOException {
+        // ProcessBuilder builder = new ProcessBuilder("java", "-cp", classpath,
+        // args[0], args[1]);
+        ProcessBuilder builder = new ProcessBuilder(args);
+        builder.redirectErrorStream(true);
+
+        final Process p = builder.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String line = null;
+                final BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        p.getInputStream()));
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    p.waitFor();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        /** ignore on close */
+                    }
+                }
+            }
+        }).start();
+    }
 }
