5a5da92a7bc06d992f930a2f7ac2ecfdf47c332d
ctr <christian.trummer@chello.at>
2014-10-02 10:24:15 +0200

    Windows classpath

--- ActiveMQTest/src/TestStarter.java
+++ ActiveMQTest/src/TestStarter.java
@@ -4,79 +4,86 @@ import java.io.InputStreamReader;
 
 public class TestStarter {
 
-    private final static String baseDir = "/Users/ctrummer/git/projects/ActiveMQTest/";
+	private final static String baseDir = "/Users/ctrummer/git/projects/ActiveMQTest/";
 
-    private final static String classpath = "./:" + "./bin/:" + // baseDir + ";" + baseDir + "bin/;"
-                                                                // +
-                                                                // //
-            "./lib/geronimo-jms_1.1_spec-1.1.jar:" + //
-            "./lib/qpid-amqp-1-0-client-0.26.jar:" + //
-            "./lib/qpid-amqp-1-0-client-jms-0.26.jar:" + //
-            "./lib/qpid-amqp-1-0-common-0.26.jar:";
+	private final static String classpath = "./;" + "./bin/;" + // baseDir + ";"
+																// + baseDir +
+																// "bin/;"
+																// +
+																// //
+			"./lib/geronimo-jms_1.1_spec-1.1.jar;" + //
+			"./lib/qpid-amqp-1-0-client-0.26.jar;" + //
+			"./lib/qpid-amqp-1-0-client-jms-0.26.jar;" + //
+			"./lib/qpid-amqp-1-0-common-0.26.jar;";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        OutputWriter out = new OutputWriter("./log/TestStarter.txt");
-        out.writeln("TestStarter started.");
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
+		OutputWriter out = new OutputWriter("./log/TestStarter.txt");
+		out.writeln("TestStarter started.");
 
-        System.out.println("cp == " + classpath);
+		System.out.println("cp == " + classpath);
 
-        for (int listenerCounter = 1; listenerCounter <= Configuration.numberOfSubsribers; listenerCounter++) {
-            if (listenerCounter != Configuration.numberOfSubsribers) {
-                createSubVm("java", "-cp", classpath, "Listener",
-                        Configuration.destination_listener, "Listener" + listenerCounter, "all");
-            } else {
-                createSubVm("java", "-cp", classpath, "Listener",
-                        Configuration.destination_listener, "Listener" + listenerCounter, "1");
-            }
+		for (int listenerCounter = 1; listenerCounter <= Configuration.numberOfSubsribers; listenerCounter++) {
+			if (listenerCounter != Configuration.numberOfSubsribers) {
+				createSubVm("java", "-cp", classpath, "Listener",
+						Configuration.destination_listener, "Listener"
+								+ listenerCounter, "all");
+			} else {
+				createSubVm("java", "-cp", classpath, "Listener",
+						Configuration.destination_listener, "Listener"
+								+ listenerCounter, "1");
+			}
 
-            out.writeln("Listener number " + listenerCounter + " started.");
-        }
+			out.writeln("Listener number " + listenerCounter + " started.");
+		}
 
-        for (int publisherCounter = 1; publisherCounter <= Configuration.numberOfPublishers; publisherCounter++) {
-            createSubVm("java", "-cp", classpath, "ListenerPublisher", "" + publisherCounter);
-            System.out.println("Publisher number " + publisherCounter + " started.");
-        }
+		for (int publisherCounter = 1; publisherCounter <= Configuration.numberOfPublishers; publisherCounter++) {
+			createSubVm("java", "-cp", classpath, "ListenerPublisher", ""
+					+ publisherCounter);
+			System.out.println("Publisher number " + publisherCounter
+					+ " started.");
+		}
 
-        Thread.sleep(10000);
+		Thread.sleep(10000);
 
-        createSubVm("java", "-cp", classpath, "PublishersStarter");
+		createSubVm("java", "-cp", classpath, "PublishersStarter");
 
-        out.writeln("PublisherStarter started.");
-        out.writeln("TestStarter finished.");
-        out.close();
-    }
+		out.writeln("PublisherStarter started.");
+		out.writeln("TestStarter finished.");
+		out.close();
+	}
 
-    public static void createSubVm(String... args) throws IOException {
-        // ProcessBuilder builder = new ProcessBuilder("java", "-cp", classpath,
-        // args[0], args[1]);
-        ProcessBuilder builder = new ProcessBuilder(args);
-        builder.redirectErrorStream(true);
+	public static void createSubVm(String... args) throws IOException {
+		// ProcessBuilder builder = new ProcessBuilder("java", "-cp", classpath,
+		// args[0], args[1]);
+		ProcessBuilder builder = new ProcessBuilder(args);
+		builder.redirectErrorStream(true);
 
-        final Process p = builder.start();
+		final Process p = builder.start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String line = null;
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        p.getInputStream()));
-                try {
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                    p.waitFor();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        /** ignore on close */
-                    }
-                }
-            }
-        }).start();
-    }
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String line = null;
+				final BufferedReader reader = new BufferedReader(
+						new InputStreamReader(p.getInputStream()));
+				try {
+					while ((line = reader.readLine()) != null) {
+						System.out.println(line);
+					}
+					p.waitFor();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						/** ignore on close */
+					}
+				}
+			}
+		}).start();
+	}
 }
