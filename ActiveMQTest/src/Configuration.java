bf12359b61b07ac3a17357b40c3436d7a2625137
ctrummer <c.trummer@ssi-schaefer-peem.com>
2014-10-13 21:33:25 +0200

    Copy together (ActiveMQ, HornetQ) into OpenMQ Configuration.java

--- ActiveMQTest/src/Configuration.java
+++ ActiveMQTest/src/Configuration.java
@@ -66,3 +66,108 @@ public class Configuration {
 	}
 
 }
+
+
+// Listener ActiveMQ
+
+private void initListener() throws JMSException {
+    factory = new ConnectionFactoryImpl(Configuration.host,
+            Configuration.port, Configuration.user, Configuration.password);
+
+    connection = factory.createConnection(Configuration.user,
+            Configuration.password);
+    connection.setClientID(listenerID);
+    connection.start();
+
+    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+    if (filter.equalsIgnoreCase("all")) {
+        // consumer = session.createConsumer(dest);
+        consumer = session.createDurableSubscriber(dest, listenerID);
+    } else {
+        // consumer = session.createConsumer(dest, "publisher = '" + filter
+        // + "'");
+        consumer = session.createDurableSubscriber(dest, listenerID,
+                "publisher = '" + filter + "'", false);
+
+    }
+}
+
+// Listener HornetQ
+private void initListener() throws JMSException {
+    TransportConfiguration transportConfiguration = new TransportConfiguration(
+            NettyConnectorFactory.class.getName());
+    factory = HornetQJMSClient.createConnectionFactoryWithoutHA(
+            JMSFactoryType.TOPIC_CF, transportConfiguration);
+
+    // factory = new HornetQConnectionFactory(Configuration.host,
+    // Configuration.port, Configuration.user, Configuration.password);
+
+    connection = factory.createConnection(Configuration.user,
+            Configuration.password);
+    connection.setClientID(listenerID);
+    connection.start();
+
+    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+    if (filter.equalsIgnoreCase("all")) {
+        // consumer = session.createConsumer(dest);
+        consumer = session.createDurableConsumer(dest, listenerID);
+        // consumer = session.createDurableSubscriber(dest, listenerID);
+    } else {
+        // consumer = session.createConsumer(dest, "publisher = '" + filter
+        // + "'");
+        consumer = session.createDurableConsumer(dest, listenerID,
+                "publisher = '" + filter + "'", false);
+        // consumer = session.createDurableSubscriber(dest, listenerID,
+        // "publisher = '" + filter + "'", false);
+
+    }
+}
+
+
+// Publisher ActieMQ
+
+//Connection
+ConnectionFactoryImpl factory = new ConnectionFactoryImpl(
+        Configuration.host, Configuration.port, Configuration.user,
+        Configuration.password);
+Connection connection = factory.createConnection(Configuration.user,
+        Configuration.password);
+connection.start();
+
+// Session Mode !!!
+Session session = connection.createSession(false,
+        Session.CLIENT_ACKNOWLEDGE);
+
+// Producer
+MessageProducer producer = session.createProducer(Configuration
+        .getDestination(Configuration.destination_listener));
+producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+// send a bulk of messages
+sendMessageBulk(session, producer);
+
+
+// Publisher HornetQ
+TransportConfiguration transportConfiguration = new TransportConfiguration(
+        NettyConnectorFactory.class.getName());
+factory = HornetQJMSClient.createConnectionFactoryWithoutHA(
+        JMSFactoryType.TOPIC_CF, transportConfiguration);
+
+Connection connection = factory.createConnection(Configuration.user,
+        Configuration.password);
+connection.start();
+
+// Session Mode !!!
+Session session = connection.createSession(false,
+        Session.CLIENT_ACKNOWLEDGE);
+
+// Producer
+MessageProducer producer = session.createProducer(Configuration
+        .getDestination(Configuration.destination_listener));
+producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+// send a bulk of messages
+sendMessageBulk(session, producer);
+
