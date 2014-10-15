87258e05760ff74c4721eacc9642da788df72767
ctr <christian.trummer@chello.at>
2014-10-15 10:29:55 +0200

    OPenMQ ready

--- ActiveMQTest/src/Configuration.java
+++ ActiveMQTest/src/Configuration.java
@@ -4,22 +4,22 @@ import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 
-import org.apache.qpid.amqp_1_0.jms.impl.ConnectionFactoryImpl;
-import org.apache.qpid.amqp_1_0.jms.impl.TopicImpl;
+import com.sun.messaging.ConnectionFactory;
+import com.sun.messaging.Topic;
 
 public class Configuration {
 	public static final String user = "admin";
-	public static final String password = "password";
+	public static final String password = "admin";
 	public static final String host = "localhost";
-	public static final int port = 5672;
-	public static final String destination_listener = "topic://event";
-	public static final String destination_starter = "topic://start";
+	public static final int port = 1099;
+	public static final String destination_listener = "eventTopic";
+	public static final String destination_starter = "startTopic";
 	public static final int numberOfTestMessages = 1000;
 	public static final int sizeOfTestMessages = 256;
 
 	private static final String DATA = "abcdefghijklmnopqrstuvwxyz";
-	public static int numberOfPublishers = 128;
-	public static int numberOfSubsribers = 128;
+	public static int numberOfPublishers = 32;
+	public static int numberOfSubsribers = 32;
 
 	public static String createMessageText(int messageSize) {
 		String messageText = "";
@@ -31,71 +31,71 @@ public class Configuration {
 
 	public static Destination getDestination(String destination)
 			throws JMSException {
-		// return getDestinationOpenMQ(destination);
-		return getDestinationActiveMQ(destination);
+		return getDestinationOpenMQ(destination);
+		// return getDestinationActiveMQ(destination);
 	}
 
-	// private static Destination getDestinationOpenMQ(String destination)
-	// throws JMSException {
-	// return new Topic(destination);
-	// }
-
-	private static Destination getDestinationActiveMQ(String destination)
+	private static Destination getDestinationOpenMQ(String destination)
 			throws JMSException {
-		return new TopicImpl(destination);
+		return new Topic(destination);
 	}
+
+	// private static Destination getDestinationActiveMQ(String destination)
+	// throws JMSException {
+	// return new TopicImpl(destination);
+	// }
 
 	public static Connection getConnection() throws JMSException {
-		// return getConnectionOpenMQ();
-		return getConnectionActiveMQ();
+		return getConnectionOpenMQ();
+		// return getConnectionActiveMQ();
 	}
 
-	// private static Connection getConnectionOpenMQ() throws JMSException {
-	// ConnectionFactory factory = new ConnectionFactory();
-	// Connection connection = factory.createTopicConnection();
-	// return connection;
-	// }
-
-	private static Connection getConnectionActiveMQ() throws JMSException {
-		ConnectionFactoryImpl factory = new ConnectionFactoryImpl(
-				Configuration.host, Configuration.port, Configuration.user,
-				Configuration.password);
-		Connection connection = factory.createConnection(Configuration.user,
-				Configuration.password);
+	private static Connection getConnectionOpenMQ() throws JMSException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.createTopicConnection();
 		return connection;
 	}
+
+	// private static Connection getConnectionActiveMQ() throws JMSException {
+	// ConnectionFactoryImpl factory = new ConnectionFactoryImpl(
+	// Configuration.host, Configuration.port, Configuration.user,
+	// Configuration.password);
+	// Connection connection = factory.createConnection(Configuration.user,
+	// Configuration.password);
+	// return connection;
+	// }
 
 	public static MessageConsumer createConsumer(Session session,
 			String listenerID, javax.jms.Topic dest, String filter)
 			throws JMSException {
-		// return createConsumerOpenMQ(session, listenerID, dest, filter);
-		return createConsumerActiveMQ(session, listenerID, dest, filter);
+		return createConsumerOpenMQ(session, listenerID, dest, filter);
+		// return createConsumerActiveMQ(session, listenerID, dest, filter);
 	}
 
-	// private static MessageConsumer createConsumerOpenMQ(Session session,
-	// String listenerID, javax.jms.Topic dest, String filter)
-	// throws JMSException {
-	// MessageConsumer consumer;
-	// if (filter.equalsIgnoreCase("all")) {
-	// consumer = session.createDurableConsumer(dest, listenerID);
-	// } else {
-	// consumer = session.createDurableConsumer(dest, listenerID,
-	// "publisher = '" + filter + "'", false);
-	// }
-	// return consumer;
-	// }
-
-	private static MessageConsumer createConsumerActiveMQ(Session session,
+	private static MessageConsumer createConsumerOpenMQ(Session session,
 			String listenerID, javax.jms.Topic dest, String filter)
 			throws JMSException {
 		MessageConsumer consumer;
 		if (filter.equalsIgnoreCase("all")) {
-			consumer = session.createDurableSubscriber(dest, listenerID);
+			consumer = session.createDurableConsumer(dest, listenerID);
 		} else {
-			consumer = session.createDurableSubscriber(dest, listenerID,
+			consumer = session.createDurableConsumer(dest, listenerID,
 					"publisher = '" + filter + "'", false);
 		}
 		return consumer;
 	}
+
+	// private static MessageConsumer createConsumerActiveMQ(Session session,
+	// String listenerID, javax.jms.Topic dest, String filter)
+	// throws JMSException {
+	// MessageConsumer consumer;
+	// if (filter.equalsIgnoreCase("all")) {
+	// consumer = session.createDurableSubscriber(dest, listenerID);
+	// } else {
+	// consumer = session.createDurableSubscriber(dest, listenerID,
+	// "publisher = '" + filter + "'", false);
+	// }
+	// return consumer;
+	// }
 
 }
