3e62e85fe94045279dedb6d19c2da8cc095bce45
Christian Trummer (PC) <christian.trummer@chello.at>
2018-11-28 20:11:54 +0100

    JUnit without Maven - functional state.

---
+++ JavaRestartTraining/test/com/trummer/javarestarttraining/business/entities/EmptyTest.java
@@ -0,0 +1,26 @@
+package com.trummer.javarestarttraining.business.entities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class EmptyTest {
+
+	@BeforeAll
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@AfterEach
+	protected void tearDown() throws Exception {
+	}
+
+}
