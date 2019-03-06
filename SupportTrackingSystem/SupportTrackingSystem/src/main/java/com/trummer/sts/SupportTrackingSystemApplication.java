03627dcbcff8e3628fc41b180ac4716dd7828cd2
Christian Trummer (PC) <christian.trummer@chello.at>
2019-03-06 19:09:40 +0100

    Initial import to GIT repository.

---
+++ SupportTrackingSystem/SupportTrackingSystem/src/main/java/com/trummer/sts/SupportTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trummer.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTrackingSystemApplication.class, args);
+	}
+
+}
